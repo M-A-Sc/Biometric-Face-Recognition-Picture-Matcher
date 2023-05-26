5e4e6fd9462784eb6674a1638d987bd5e60a5317
M-A-Sc <134670822+M-A-Sc@users.noreply.github.com>
2023-05-26 21:53:54 +0200

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -1,41 +1,29 @@
-#M-A-Sc
+#M-A-Sc#
+
+ 
+import face_recognition
+import argparse
 
 
-import cv2
+# Argumente parsen
+parser = argparse.ArgumentParser()
+parser.add_argument("--sample", required=True, help="Pfad zur Sample-Datei")
+parser.add_argument("--template", required=True, help="Pfad zur Template-Datei")
+args = parser.parse_args()
 
-# Bildpfad
-image_path = "example_image.jpg"
+# Einlesen der Bilder
+sample_image = face_recognition.load_image_file(args.sample)
+template_image = face_recognition.load_image_file(args.template)
 
-# Lade Bild
-image = cv2.imread(image_path)
+# Auslesen der biometrischen Merkmale
+sample_encoding = face_recognition.face_encodings(sample_image)[0]
+template_encoding = face_recognition.face_encodings(template_image)[0]
 
-# Zeige Originalbild an und warte auf Tastendruck
-cv2.imshow("Originalbild", image)
-cv2.waitKey(0)
+# Vergleich der Merkmale
+results = face_recognition.compare_faces([sample_encoding], template_encoding)
 
-# Konvertiere das Bild in Graustufen
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Initialisiere den Gesichtserkennungs-Algorithmus
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-
-# Gesichtserkennung auf dem Graustufenbild durchführen
-faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5)
-
-# Zeichne Rechteck um das erkannte Gesicht
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)    
-
-# Zeige das Bild mit Gesichtserkennung an
-cv2.imshow("Gesichtserkennung", image)
-
-# Speichere die detektierten Gesichtsregionen als Bilddateien
-for i, (x, y, w, h) in enumerate(faces):
-    face_image = image[y:y+h, x:x+w]
-    cv2.imwrite(f"face_{i}.jpg", face_image)    
-
-# Warte auf Tastendruck zum Schließen
-cv2.waitKey(0)
-
-# Schließe das Fenster
-cv2.destroyAllWindows()
+# Ergebnis
+if results[0]:
+    print("Die Bilder matchen (accept)")
+else:
+    print("Die Bilder matchen nicht (reject)")
